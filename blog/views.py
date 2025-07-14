94a6142556234105dd57792f5f3355de10af3325
Zach Hamby <zhamby1@ivytech.edu>
2025-07-14 10:39:49 -0400

    added css and template extentions

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,3 +9,9 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #if we want to pass a query result to the html file, we will bind a python variable with a variable the html can us
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request,pk):
+    #grabs post from database based on primary key.
+    #pk = pk means that the primary key passed to this url must grab the pk that matches in the database
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
